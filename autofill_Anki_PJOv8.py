3cfcea6866a5e72f5050cfdb72c616af87470c81
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-08-25 18:22:13 -0300

    update code

--- autofill_Anki_PJOv8.py
+++ autofill_Anki_PJOv8.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -5,34 +11,49 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
-import minha_senha
+import infos
+from tkinter import *
+from tkinter import messagebox
+
 
 link_licao = input('Insira o link da aula: ')
 
 tipo_de_licao = input('''Digite 1 se essa lição deve ser inserido no deck de Escrita Japonesa
 Digite 2 se essa lição deve ser inserido no deck de Frases em Japonês: ''')
 
-site_lento = input('O site está lento? y/n ').casefold()
-
-tempo_espera = 15 if site_lento =='y' else 5
-
-# servico = Service(ChromeDriverManager().install())
-servico = Service(ChromeDriverManager(version='114.0.5735.90').install())
+servico = Service()
 opcoes = Options()
 opcoes.add_argument("--start-maximized")
 navegador = webdriver.Chrome(service=servico, options=opcoes)
 
+
+# In[8]:
+
+
+def esperar_carregar(elemento):
+    while len(navegador.find_elements(By.CLASS_NAME, elemento)) < 1:
+        time.sleep(1)
+    time.sleep(2)
+
+
+# In[9]:
+
+
 #login no site da PJO
 navegador.get(r'https://portal.programajaponesonline.com.br/login/')
-time.sleep(tempo_espera)
-navegador.find_element(By.ID, 'user_login').send_keys(minha_senha.login_jpo)
-navegador.find_element(By.ID, 'user_pass').send_keys(minha_senha.senha_jpo)
+esperar_carregar('input')
+navegador.find_element(By.ID, 'user_login').send_keys(infos.login_jpo)
+navegador.find_element(By.ID, 'user_pass').send_keys(infos.senha_jpo)
 navegador.find_element(By.ID, 'wp-submit').click()
-time.sleep(tempo_espera)
+esperar_carregar('menu-item')
+
+
+# In[10]:
+
 
 #entra na lição
 navegador.get(link_licao)
-time.sleep(tempo_espera)
+esperar_carregar('single-box-content')
 
 lista_botao = navegador.find_elements(By.CLASS_NAME, 'expand-card')
 fronts = navegador.find_elements(By.CLASS_NAME, 'card-front')
@@ -49,6 +70,10 @@ lista_frentes = [frente.text for frente in fronts]
 #pega as traseiras dos cards
 lista_tras = [tras.text for tras in backs]
 
+
+# In[11]:
+
+
 #verificando as frentes e as traseiras do cards
 print(lista_frentes)
 print(lista_tras)
@@ -58,34 +83,50 @@ for item in lista_frentes:
     if item == '':
         navegador.close()
 
+
+# In[12]:
+
+
 #verificando visualmente qual a quantidade de frases que serão adicionadas no Anki
 print(len(lista_frentes))
 print(len(lista_tras))
 
+
+# In[13]:
+
+
 #login no ankiweb
 navegador.get(r'https://ankiweb.net/account/login')
-time.sleep(tempo_espera)
+esperar_carregar('form-control')
 campos = navegador.find_elements(By.CLASS_NAME, 'form-control')
-campos[0].send_keys(minha_senha.login_anki)
-campos[1].send_keys(minha_senha.senha_anki)
+campos[0].send_keys(infos.login_anki)
+campos[1].send_keys(infos.senha_anki)
 navegador.find_element(By.XPATH, '/html/body/div/main/form/div[3]/button').click()
-time.sleep(tempo_espera)
+esperar_carregar('btn-outline-secondary')
 navegador.get(r'https://ankiuser.net/edit/')
-time.sleep(tempo_espera)
+esperar_carregar('form-group')
+
+
+# In[14]:
+
 
 if tipo_de_licao == '1':
     #opção Escrita Japonesa
-    navegador.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/div[2]/input').send_keys('Escrita em Japonês', Keys.TAB)
+    navegador.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/div[2]/input').send_keys(infos.decks[0], Keys.TAB)
 
 elif tipo_de_licao == '2':
     #opção Frases em Japonês
-    navegador.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/div[2]/input').send_keys('Frases em Japonês', Keys.TAB)
+    navegador.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/div[2]/input').send_keys(infos.decks[1], Keys.TAB)
 
 else:
     #deck de testes
-    navegador.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/div[2]/input').send_keys('testes', Keys.TAB)
+    navegador.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/div[2]/input').send_keys(infos.decks[2], Keys.TAB)
 
 time.sleep(3)
+
+
+# In[15]:
+
 
 #criando o card
 for i, botao in enumerate(lista_botao):
@@ -102,5 +143,13 @@ for i, botao in enumerate(lista_botao):
         navegador.find_element(By.XPATH, '/html/body/div/main/form/button').click()
         time.sleep(3)
 
+
+# In[16]:
+
+
 #fechando o navegador
 navegador.close()
+janela = Tk()
+messagebox.showinfo('Programa concluído com sucesso. Os seus cards foram criados!.')
+janela.destroy()
+
